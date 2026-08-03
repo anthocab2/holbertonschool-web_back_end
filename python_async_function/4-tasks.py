a48aed99a7190b026962b9d62f4c98d30dcd9d08
anthocab2 <anthonycaban1010@gmail.com>
2026-08-02 20:20:27 -0400

    Create task based concurrent coroutine execution]

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Execute multiple asyncio tasks."""
+"""Create multiple asyncio tasks."""
 
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
-    """Execute tasks and return delays in ascending order."""
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Return delays from multiple concurrent tasks."""
     delays = []
 
     tasks = []
