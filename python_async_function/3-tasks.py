c9dbfe66b292f7d6b1b815b105dac20d26acecac
anthocab2 <anthonycaban1010@gmail.com>
2026-08-02 20:18:29 -0400

    Create asyncio task for wait_random coroutine

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
-    """Return an asyncio Task for wait_random coroutine."""
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return an asyncio Task that runs wait_random."""
     return asyncio.create_task(wait_random(max_delay))
