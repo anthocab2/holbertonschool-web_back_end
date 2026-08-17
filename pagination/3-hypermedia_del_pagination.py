6f130436a4bdf7a2b6135e43a999a01e71636f3e
anthocab2 <anthonycaban1010@gmail.com>
2026-08-16 23:05:16 -0400

    Fix deletion-resilient pagination parameter format

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -34,10 +34,10 @@ class Server:
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
             }
+
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a deletion-resilient page starting at the given index."""
         if index is None:
             index = 0
