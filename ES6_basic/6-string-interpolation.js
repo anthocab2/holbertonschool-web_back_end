8e1b9f4a6171a485f6536024880eebe27d083f2e
anthocab2 <anthonycaban1010@gmail.com>
2026-08-26 16:12:37 -0400

    Use template literals for string interpolation

---
+++ ES6_basic/6-string-interpolation.js
@@ -0,0 +1,8 @@
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const income = 476.39;
+  const gdp = 112.7;
+  const capita = 178717;
+
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of $${income}. As of 2015, San Francisco proper had a GDP of $${gdp} billion, and a GDP per capita of $${capita}.`;
+}
