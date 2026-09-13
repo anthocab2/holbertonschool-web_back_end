cf1a4249bb28f696f06bb783ab935664b634edf8
anthocab2 <anthonycaban1010@gmail.com>
2026-09-13 15:08:54 -0400

    Fix stdin handling for child process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,15 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
